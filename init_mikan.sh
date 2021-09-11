8a0a05b95367a2d83981de84692344a3425787bd
three <moresuke0@gmail.com>
2021-09-11 17:56:17 +0900

    Update init_mikan.sh

--- init_mikan.sh
+++ init_mikan.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
-
 echo -e "Initialization started\n"
 
-cd ~/edk2
-rm MikanLoaderPkg
+pushd ~/edk2
+rm MikanLoaderPkg || true
 ln -s $OS_DIR/MikanLoaderPkg ./
 source edksetup.sh
 cp /workspaces/mikanos-devcontainer/target.txt Conf/target.txt
+source ~/osbook/devenv/buildenv.sh
 echo -e "\nSuccessfully initialized"
+popd
