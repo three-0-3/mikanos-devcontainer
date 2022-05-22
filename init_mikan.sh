0fd3dbbcb390f3457724d4eca5689b801eb9806e
three <moresuke0@gmail.com>
2022-05-22 19:07:48 +0900

    Add command alias

--- init_mikan.sh
+++ init_mikan.sh
@@ -8,5 +8,10 @@ ln -s $OS_DIR/MikanLoaderPkg ./
 source edksetup.sh
 cp /workspaces/mikanos-devcontainer/target.txt Conf/target.txt
 source ~/osbook/devenv/buildenv.sh
+alias mikan='(cd $OS_DIR/kernel; export OPTMCONF="-O2"; make; cd /workspaces/mikanos-devcontainer; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
+alias dmikan='(cd $OS_DIR/kernel; export OPTMCONF="-O0"; make; cd /workspaces/mikanos-devcontainer; export QEMU_OPTS="-s -S"; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
+alias mikanb='(cd $OS_DIR/kernel; export OPTMCONF="-O2"; make -B; cd /workspaces/mikanos-devcontainer; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
+alias dmikanb='(cd $OS_DIR/kernel; export OPTMCONF="-O0"; make -B; cd /workspaces/mikanos-devcontainer; export QEMU_OPTS="-s -S"; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
+alias gdbmikan='gdb -x /workspaces/mikanos-devcontainer/debug_mikan'
 echo -e "\nSuccessfully initialized"
 popd
