bede2b5b00792d5ad7cc1a5b9f493c382337e8ad
three <moresuke0@gmail.com>
2022-07-21 19:24:36 +0900

    Fix: mikanos pause and keep restart after edit

--- init_mikan.sh
+++ init_mikan.sh
@@ -10,7 +10,7 @@ cp /workspaces/mikanos-devcontainer/target.txt Conf/target.txt
 source ~/osbook/devenv/buildenv.sh
 alias mikan='(cd $OS_DIR/kernel; export OPTMCONF="-O2"; make; cd /workspaces/mikanos-devcontainer; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
 alias dmikan='(cd $OS_DIR/kernel; export OPTMCONF="-O0"; make; cd /workspaces/mikanos-devcontainer; export QEMU_OPTS="-s -S"; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
-alias mikanb='(cd $OS_DIR/kernel; export OPTMCONF="-O2"; make -B; cd /workspaces/mikanos-devcontainer; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
+alias mikanb='(cd $OS_DIR/kernel; export OPTMCONF="-O2"; make clean; make -B; cd /workspaces/mikanos-devcontainer; rm disk.img; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
 alias dmikanb='(cd $OS_DIR/kernel; export OPTMCONF="-O0"; make -B; cd /workspaces/mikanos-devcontainer; export QEMU_OPTS="-s -S"; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
 alias gdbmikan='gdb -x /workspaces/mikanos-devcontainer/debug_mikan'
 echo -e "\nSuccessfully initialized"
