1f9533e1b06ff8a33bb0f463c9cf3d03401d6bff
three <moresuke0@gmail.com>
2022-07-30 17:12:24 +0000

    Apply patch for Apple Silicon

--- init_mikan.sh
+++ init_mikan.sh
@@ -6,6 +6,11 @@ pushd ~/edk2
 rm MikanLoaderPkg || true
 ln -s $OS_DIR/MikanLoaderPkg ./
 source edksetup.sh
+if [ $(uname -p) == "aarch64" ]; then
+	EDKTOOLS="$HOME/edk2/Conf/tools_def.txt"
+  sed -i s/^'DEFINE CLANG38_X64_TARGET'.*/'DEFINE CLANG38_X64_TARGET           = -target x86_64-linux-gnu'/ ${EDKTOOLS}
+  sed -i s/^'DEBUG_CLANG38_X64_CC_FLAGS'.*/'DEBUG_CLANG38_X64_CC_FLAGS         = DEF(CLANG38_ALL_CC_FLAGS) -m64 "-DEFIAPI=__attribute__((ms_abi))" -mno-red-zone -mcmodel=small -fpie -Oz -flto DEF(CLANG38_X64_TARGET) -g -I\/usr\/x86_64-linux-gnu\/include'/ ${EDKTOOLS}
+fi
 cp /workspaces/mikanos-devcontainer/target.txt Conf/target.txt
 source ~/osbook/devenv/buildenv.sh
 alias mikan='(cd $OS_DIR/kernel; export OPTMCONF="-O2"; make; cd /workspaces/mikanos-devcontainer; ~/osbook/devenv/run_qemu.sh ~/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $OS_DIR/kernel/kernel.elf)'
